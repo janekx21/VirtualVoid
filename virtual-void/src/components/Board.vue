f1d0a8b71e762118833876b5dad9587067253c44
Janek Winkler <janekx21@gmail.com>
2021-09-02 23:07:36 +0200

    stzff

---
+++ virtual-void/src/components/Board.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex justify-between space-x-2 overflow-hidden rounded-3xl">
+    <div class="flex flex-col color-state">
+      <div class="flex justify-center text-5xl font-serif color-head p-4">TODO</div>
+      <div>
+        <Issue></Issue>
+        <Issue></Issue>
+      </div>
+    </div>
+    <div class="flex flex-col color-state">
+      <div class="flex justify-center text-5xl font-serif color-head p-4">Doing</div>
+      <div>
+        <Issue></Issue>
+      </div>
+    </div>
+    <div class="flex flex-col color-state">
+      <div class="flex justify-center text-5xl font-serif color-head p-4">Done</div>
+      <div>
+        <Issue></Issue>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Issue from "./Issue.vue"
+defineProps<{ msg: string }>()
+</script>
+
+<style scoped>
+  .color-state {
+    background-color: #959595;
+  }
+
+  .color-head {
+    color: #5e5e5e;
+    background-color: #c3c3c3;
+  }
+</style>
---
+++ virtual-void/src/components/Issue.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="flex flex-col m-4 rounded-3xl overflow-hidden font-sans issue">
+    <div class="color-head flex justify-between text-2xl font-bold p-3">
+      <div class="title">Issue Titel</div>
+      <div>#123</div>
+    </div>
+    <div class="color-body text-base overflow-hidden p-3">
+      <i class="gg-arrow-top-right-o"></i>
+      <p class="description">
+        Issue Description foo bar foo bar foo bar foo bar foo bar foo bar foo bar
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Issue"
+}
+</script>
+
+<style scoped>
+
+.issue {
+  background-color: #c8c8c8;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.30), 0 0 0 rgba(0, 0, 0, 0.22);
+  transition: all .2s cubic-bezier(0, 0, 0.2, 1);
+  transform: translate(0px, 0px);
+}
+
+.issue:hover {
+  box-shadow: 0 19px 38px rgba(0, 0, 0, 0.30), 0 15px 12px rgba(0, 0, 0, 0.22);
+  transform: translate(0px, -5px) scale(1.02);
+  cursor: pointer;
+}
+
+:host div {
+  padding: 0 1rem;
+}
+
+.color-head {
+  background-color: #dddddd;
+  color: #515151;
+}
+
+.color-body {
+  color: #373737;
+}
+
+.title {
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  flex-shrink: 1;
+  overflow: hidden;
+}
+
+.description {
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+}
+</style>
